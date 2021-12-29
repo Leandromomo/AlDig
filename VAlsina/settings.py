673ba767b7aa0d491041a858b95e5f17361851c7
LeAnDrO <83434204+Leandromomo@users.noreply.github.com>
2021-12-29 20:36:15 -0300

    Update settings.py

--- VAlsina/settings.py
+++ VAlsina/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="leandrobascones@gmail.com"
-EMAIL_HOST_PASSWORD="Ts1234qw"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
