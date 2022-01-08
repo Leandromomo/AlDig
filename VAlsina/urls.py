e026740456e34f045aecd6600c788af100489107
LeAnDrO <83434204+Leandromomo@users.noreply.github.com>
2022-01-08 00:26:33 -0300

    Add files via upload

--- VAlsina/urls.py
+++ VAlsina/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,6 +36,6 @@ urlpatterns = [
 
     path('registro/', include ('login.urls')),
 
-]
+]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
